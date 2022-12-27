e31fef69c3ac831fc9153e826481c4a577869914
FrancoAJohnson <frankiejknowsbest@icloud.com>
2022-12-26 19:02:17 -0500

    divide without operator

---
+++ src/WeekOneGroupTask/week1MentorTask.java
@@ -0,0 +1,16 @@
+package WeekOneGroupTask;
+
+public class week1MentorTask {
+    public static void main(String[] args) {
+
+
+        int number = 45;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
+}
